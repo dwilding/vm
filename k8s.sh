e66aa0295ab5c5ecb66c00456885549165e41166
David Wilding <david.wilding@canonical.com>
2025-04-22 15:19:27 +0800

    add script for K8s VMs

---
+++ k8s.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sudo snap install --classic concierge
+sudo concierge prepare -p microk8s
+sudo microk8s enable registry
+
+cd
+wget https://download.stateful.com/runme/3.13.1/runme_linux_x86_64.deb
+sudo apt install ./runme_linux_x86_64.deb
+rm runme_linux_x86_64.deb
+cat <<EOF > .bash_aliases
+alias runme='runme --filename=README.md --allow-unnamed=false --exit'
+EOF
