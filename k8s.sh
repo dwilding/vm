114e0323ca60a12bd47fa0e8fc0e575e2cc8e6b1
David Wilding <david.wilding@canonical.com>
2025-04-25 11:13:32 +0800

    fixed bugs in setup script

--- k8s.sh
+++ k8s.sh
@@ -7,7 +7,7 @@ sudo microk8s enable registry
 cd
 wget https://download.stateful.com/runme/3.13.1/runme_linux_x86_64.deb
 sudo apt install ./runme_linux_x86_64.deb
-rm runme_linux_x86_64.deb
+rm -f runme_linux_x86_64.deb
 cat <<EOF > .bash_aliases
 alias runme='runme --filename=README.md --allow-unnamed=false --exit'
 EOF
