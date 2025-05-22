00a79225f007e61126c3454134cbf1b9ef35178b
David Wilding <david.wilding@canonical.com>
2025-05-22 16:03:24 +0800

    update setup script

--- k8s.sh
+++ k8s.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+# Install charming tools
 sudo snap install --classic concierge
 sudo concierge prepare -p microk8s
 sudo microk8s enable registry
 
+# Install uv
 sudo snap install --classic astral-uv
 
+# Install runme (for running commands in README files)
 cd
 wget https://download.stateful.com/runme/3.13.1/runme_linux_x86_64.deb
 sudo apt install ./runme_linux_x86_64.deb
@@ -13,3 +16,8 @@ rm -f runme_linux_x86_64.deb
 cat <<EOF > .bash_aliases
 alias runme='runme --filename=README.md --allow-unnamed=false --exit'
 EOF
+
+# Clone repos that contain Kubernetes charms
+cd
+git clone https://github.com/canonical/operator.git
+git clone https://github.com/canonical/dashboard.git
