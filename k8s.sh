9ebe1af0a580094ba612e24368ce6d4e6e48442c
David Wilding <david.wilding@canonical.com>
2025-05-22 15:43:56 +0800

    install uv

--- k8s.sh
+++ k8s.sh
@@ -4,6 +4,8 @@ sudo snap install --classic concierge
 sudo concierge prepare -p microk8s
 sudo microk8s enable registry
 
+sudo snap install --classic astral-uv
+
 cd
 wget https://download.stateful.com/runme/3.13.1/runme_linux_x86_64.deb
 sudo apt install ./runme_linux_x86_64.deb
