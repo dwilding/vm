ef6e035b9687c7542b29a561ac44c4973c65490b
David Wilding <david.wilding@canonical.com>
2025-07-17 16:44:37 +0100

    add gimmegit, remove charms

--- k8s.sh
+++ k8s.sh
@@ -12,16 +12,24 @@ uv tool update-shell
 # Install tox and the tox-uv plugin
 uv tool install tox --with tox-uv
 
+# Install gimmegit (for cloning fully-isolated branches)
+uv tool install gimmegit
+cat <<'EOF' >> ~/.bashrc
+
+gimmegit() {
+    __="$(
+        PYTHONUNBUFFERED=1 \
+        command gimmegit "$@" | tee /dev/tty | tail -n 1
+    )"
+}
+EOF
+
 # Install runme (for running commands in README files)
 cd
 wget https://download.stateful.com/runme/3.13.1/runme_linux_x86_64.deb
 sudo apt install ./runme_linux_x86_64.deb
 rm -f runme_linux_x86_64.deb
-cat <<EOF > .bash_aliases
+cat <<'EOF' >> ~/.bash_aliases
+
 alias runme='runme --filename=README.md --allow-unnamed=false --exit'
 EOF
-
-# Clone repos that contain Kubernetes charms
-cd
-git clone https://github.com/canonical/operator.git
-git clone https://github.com/canonical/dashboard.git
